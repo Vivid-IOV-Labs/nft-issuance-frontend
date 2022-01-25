666ee83b20029acd6e467952feb0be20410b7d8e
bolladaniele <bolladaniele@gmail.com>
2022-01-25 15:25:58 +0000

    removes vue axe

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
-import { createApp, h, Fragment } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import router from "./router";
 import store from "./store";
-import VueAxe, { VueAxePopup } from "vue-axe";
 import VueAnnouncer from "@vue-a11y/announcer";
 
-if (process.env.NODE_ENV === "development") {
-    createApp({
-        render: () => h(Fragment, [h(App), h(VueAxePopup)]),
-    })
-        .use(VueAxe)
-        .use(VueAnnouncer)
-        .use(router)
-        .use(store)
-        .mount("#app");
-} else {
-    createApp(App)
-        .use(VueAnnouncer)
-        .use(router)
-        .use(store)
-        .mount("#app");
-}
+createApp(App)
+    .use(VueAnnouncer)
+    .use(router)
+    .use(store)
+    .mount("#app");
